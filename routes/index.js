2546d7bf032e26fc5449d64b47777b37de5774c6
yann-catlinn <y.catrilelbun@alumnos.duoc.cl>
2020-09-09 22:06:06 -0300

    I've got fix error with mongo, i've added styles and changed Title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Hola Express' });
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
